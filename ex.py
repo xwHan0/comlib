7e42261c5b28072e9709f2bc698e271761b322fd
xwHan0 <xwhan2015@outlook.com>
2018-02-28 15:25:06 +0800

    add xapply comment

--- ex.py
+++ ex.py
@@ -1,4 +1,11 @@
 def xapply (func, *args, **xargs):
+  """Execute 'fun' with parameter list args and xargs.
+  
+  args: A tuple reprents the parameter for 'func'.
+        The last element of args can be a list represented remain parameter list.
+  xargs: Named map represents a named parameter
+  return: 'func' return value
+  """
   if type(args[-1])==list:
     args = args[0:-1] + tuple(args[-1])
   return apply(func, *args, **xargs)
