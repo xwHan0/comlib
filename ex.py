5894d6967625e4500eaa9838290bfb046c9fb7de
xwHan0 <xwhan2015@outlook.com>
2018-03-04 23:00:24 +0800

    fix bug

--- ex.py
+++ ex.py
@@ -11,5 +11,5 @@ def xapply (func, *args, **xargs):
   """
   if type(args[-1])==list:
     args = args[0:-1] + tuple(args[-1])
-  return apply(func, *args, **xargs)
+  return func(*args, **xargs)
   
