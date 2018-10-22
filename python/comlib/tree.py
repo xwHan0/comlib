d7516388c9014cdc919ee1f7790f4da6bd7fc13e
xwHan0 <xwhan2015@outlook.com>
2018-10-22 16:45:20 +0800

    add tree

---
+++ python/comlib/tree.py
@@ -0,0 +1,50 @@
+
+def reduce_deep(node, init = None, sub_get = None, pre_proc = None, post_proc = None, post = None, idx = [] ):
+    """深度优先迭代。
+    
+    Arguments:
+      node {Object} -- 树节点
+      init {Object: None} -- 初始化值
+      sub_get {Function} -- 获取子节点函数
+      pre_proc {Function: None} -- 迭代子节点前的父节点处理
+      post_proc {Function: None} -- 迭代子节点后的父节点处理
+      post {Function: None} -- 收尾处理
+      idx {List<int>: []} -- 节点序号
+    """
+
+    def reduce_iter_pre(node, sub_get, init, proc, idx):
+        # 处理当前节点
+        rst = proc(node, idx, init)
+        # 迭代子节点。叶子节点node.sub为空
+        for i, sub in sub_get(node):
+            rst = reduce_iter_pre( sub, idx.append(i), rst )
+        return rst
+        
+    def reduce_iter_post(node, sub_get, init, proc, idx):
+        rst = init
+        # 迭代子节点。叶子节点node.sub为空
+        for i, sub in sub_get(node):
+            rst = reduce_iter_post( sub, idx.append(i), rst )
+        # 处理当前节点
+        rst = proc(node, idx, rst)
+        # 返回
+        return rst
+    
+    # 初始值处理
+    if init == None:
+        if pre_post: init = pre_post(node, idx)
+        else: throw Exception('')
+    elif hashattr(init, '__call__'):
+        init = init(node)
+        
+    # 迭代处理
+    if pre_proc:
+        rst = reduce_iter_post(node, sub_get, init, post_proc, idx)
+    else:
+        rst = reduce_iter_pre(node, sub_get, init, post_proc, idx)
+    
+    # 后处理
+    if post: rst = post(rst)
+        
+    return rst
+        
