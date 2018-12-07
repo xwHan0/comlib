3fd686731654e70a36d76d14610cefa73b619110
xwHan0 <xwhan2015@outlook.com>
2018-12-07 23:55:56 +0800

    Add ChildXXX class

--- python/comlib/__init__.py
+++ python/comlib/__init__.py
@@ -3,6 +3,6 @@
 """
 
 
-from comlib.iterator import iterator
+from comlib.iterator import iterator, ChildAttr, ChildBypass, ChildFunction, ChildSub, ChildNone
 from comlib.iterators import Index, CommonIterator, Counter, LinkList, interpose
 from comlib.iterator_operator import *
