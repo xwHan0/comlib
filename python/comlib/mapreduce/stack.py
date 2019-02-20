ed27a68924d89ba4190eac22039087d39ccf36b9
xwHan0 <xwhan2015@outlook.com>
2019-02-20 14:31:21 +0800

    Modify with continue assign statement

--- python/comlib/mapreduce/stack.py
+++ python/comlib/mapreduce/stack.py
@@ -1,19 +1,12 @@
     
 PRE = 1
-ITE = 2
 POST = 3
-DONE = 4
 
 class NodeInfo:
     def __init__(self, datum=[],  sta=1, pred_idx=0 ):
-        self.datum = datum
+        self.datum, self.sta, self.pred_idx = datum, sta, pred_idx
         # self.children  define when used
-        self.sta = sta
-        # self.rst = rst
-        # self.idx = 0
-        self.pred_idx = pred_idx
         # self.succ = False  define when used
-        #self.is_post_yield = False
         # self.proc_idx = 0    define when used
         
         
