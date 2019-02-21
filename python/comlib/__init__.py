0e2eb6e952d5419f04f15ee202e8f26eddcf15b1
xwHan0 <xwhan2015@outlook.com>
2019-02-21 10:24:11 +0800

    Add comment

--- python/comlib/__init__.py
+++ python/comlib/__init__.py
@@ -9,4 +9,5 @@ from comlib.iterator_operator import *
 
 from comlib.mapreduce.core import Query
 from comlib.mapreduce.child_relationship import *
+from comlib.mapreduce.pred import Pred
 from comlib.mapreduce.proc import Proc, ProcMap, ProcReduce, ProcIter
