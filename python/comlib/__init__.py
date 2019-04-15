dfa7e953eab0cf04c93a3dd0fa9a011bf3ae1ba5
xwHan0 <xwhan2015@outlook.com>
2019-04-15 16:10:22 +0800

    Move file path

--- python/comlib/__init__.py
+++ python/comlib/__init__.py
@@ -4,15 +4,10 @@
 """
 
 
-from comlib.iterators import Index, CommonIterator, Counter, LinkList, interpose
+from comlib.iterators import  CommonIterator, Counter, LinkList, interpose
 from comlib.iterator_operator import *
 
-# from comlib.mapreduce.core import Query
-# from comlib.mapreduce.child_relationship import *
 from comlib.mapreduce.pred import Pred
 from comlib.mapreduce.proc import Proc, ProcMap, ProcReduce, ProcIter
 
 from comlib.mapreduce.qmar import Qmar
-# from comlib.mapreduce.match import Match
-from comlib.mapreduce.child import Child
-# from comlib.mapreduce.action import Action
