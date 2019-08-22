4fdc0f49fadf8e10a60b0a56962f8a7d107458fd
xwHan0 <xwhan2015@outlook.com>
2019-08-23 07:40:46 +0800

    init

--- python/test/test_iterator.py
+++ python/test/test_iterator.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append( (os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))) )
+sys.path.append( (os.path.abspath(os.path.join(os.path.dirname(__file__), './'))) )
 
 from comlib import Qmar
 from comlib import Pred, Proc
