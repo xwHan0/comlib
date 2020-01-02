6e2572da7fddc02e8e1dd55c67286285cc42d4a9
xwHan0 <xwhan2015@outlook.com>
2020-01-02 23:36:21 +0800

    1. Mask补充终止迭代条件 2. 恢复Algorithm类; 3. 中间版本上传;

--- python/scheduler/scheduler.py
+++ python/scheduler/scheduler.py
@@ -47,6 +47,15 @@ def gen_standard_order(dims=[]):
     return rst
 
 
+def order2dim(order):
+    dim = [0] * len(order[0])   # 每个维度初始化
+    for ord in order:   # 枚举每个Order元素
+        for d in range(len(dim)):   # 每个维度
+            if dim[d] < ord[d]:
+                dim[d] = ord[d] # 取最大值
+    return dim
+
+
 class Mask:
     def __init__(self, dims=[], gnum=1):
         
@@ -54,121 +63,132 @@ class Mask:
         self.gnum = [[gnum]*dim_size for dim_size in dims] if isinstance(gnum, int) else gnum
         # 统计每个维度、每个位置的Grant个数
         self.gcnt = [[0]*dim_size for dim_size in dims] 
+        # 统计每个维度还可以容纳Grant的维度量个数
+        self.dim_permit_gnum = dims.copy()
+        # 是否可以提前结束迭代
+        self.stop = False
         
     def reset(self):
-        for gcnts in self.gcnt: # 清空所有统计
-            for i in range(len(gcnts)):
-                gcnts[i] = 0
+        self.stop = False
+        for i in range(len(self.gcnt)):
+            self.dim_permit_gnum[i] = len(self.gcnt[i]) # 重新加载可以选择的维度量个数
+            for j in range(len(self.gcnt[i])):
+                self.gcnt[i][j] = 0 # 清空选择的Grant个数
                 
     def is_mask(self, pos):
-        """判断位置pos是否被选择过"""
+        """判断位置pos是否被选择过。返回(可以结束迭代标识, 当前位置是否屏蔽标识)"""
+        if self.stop:
+            return (True, False)
+
         for dim in range(len(self.gnum)):   # 按维度遍历
             if self.gcnt[dim][pos[dim]] >= self.gnum[dim][pos[dim]]:
-                return True # 只要任何一个维度Grant个数大于允许值，就判断为屏蔽
-        return False    # 所有维度Grant个数都小于允许值，才判断为不屏蔽
+                return (False, True) # 只要任何一个维度Grant个数大于允许值，就判断为屏蔽
+        return (False, False)    # 所有维度Grant个数都小于允许值，才判断为不屏蔽
         
     def mask(self, pos):
-        """统计pos位置获得一个Grant"""
+        """屏蔽位置pos，并返回是否某一个维度已经没有Grant令牌。仲裁可以停止的标识。"""
         for dim in range(len(self.gnum)):
             self.gcnt[dim][pos[dim]] += 1
+            if self.gcnt[dim][pos[dim]] >= self.gnum[dim][pos[dim]]:    # 当前dim维度pos[dim]位置已经被屏蔽
+                self.dim_permit_gnum[dim] -= 1
+                if self.dim_permit_gnum[dim] == 0:
+                    self.stop = True
+                    return True # 当前dim维度的Grant令牌已经分完。可以直接结束仲裁
+        return False    # 需要继续迭代搜索
 
 
-class Pointer:
-    def __init__(self, dims=[]):
-        self.scope = xmul(*dims)    # order长度
-        self.step = int(self.scope/xmax(*dims))   # 指针在order中的步长
-        
+class Order:
+    def __init__(self, dims=[], order=[], search_num=-1):
+        """
+        Arguments:
+        * dims: 仲裁维度列表
+            * 若为[]，则dims由order推导
+        * order: 仲裁搜索位置列表
+            * 若为[]，则由dims推导。默认采用标准的DPA搜索顺序
+        * search_num: 一次仲裁搜索的最大步长
+          * -1：搜索所有的可选范围
+          * 0: TDM搜索。仅搜索第一个位置相互正交的范围
+          * other: 用户选的的搜索范围
+        """
+        if dims != []:
+            self.scope = xmul(*dims)
+            self.order = gen_standard_order(dims)
+        elif order != []:
+            dims = order2dim(order)
+            self.scope = len(order)
+            self.order = order
+
+        self.step = int(self.scope/xmax(*dims))
+        self.ptr = 0
+        self.search_cnt = 0
+
+        if search_num == 0:
+            self.search_num = int( self.scope / xmax(*dims) )
+        elif search_num == -1:
+            self.search_num = self.scope
+        else:
+            self.search_num = search_num
+
     def pointer2index(self, ptr):
         return ptr * self.step
 
     def index2pointer(self, idx):
         return int(idx/self.step)
 
-    def search2index(self, ptr, search_cnt):
-        return (self.pointer2index(ptr) + search_cnt) % self.scope
-
-    def next_pointer(self, idx):
-        pointer = self.index2pointer(idx) 
-        return (pointer + 1) % int(self.scope / self.step)  # 更新到下一个指针
-
-
-class WPointer(Pointer):
-    def __init__(self, dims=[]):
-        super().__init__(dims)
-        self.pointer_table = [0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,1,1,1,1]
-        self.pointer_idx = 0
-
-    def next_pointer(self, idx):
-        self.pointer_idx = (self.pointer_idx + 1) % 20
-        return self.pointer_table[self.pointer_idx]
-
-
-class Algorithm:
-    def __init__(self, dims=[], order=None, tdm=False, pointer=None):
-        
-        if dims == []:
-            self.order = None   # 搜索位置顺序列表
-        elif order == None:
-            self.order = gen_standard_order(dims)
-        else:
-            self.order = order
-
-        # 每次搜索的最大范围
-        self.select_num = xmin(*dims) if tdm else len(self.order)
-
-        # 步骤指针
-        # 0: 仲裁初始化还未开始。等待调度__iter__
-        # 1: 仲裁初始化结束，开始仲裁。__iter__运行结束
-        # 2: 新指针已经找到。record已经记录到新指针
-        self.step = 0
-
-        self.pointer = WPointer(dims)    
-        self.ptr = 0    # 当前指针
-        self.next_ptr = 0   # 下一跳指针
-        self.search_cnt = 0    # 当前搜索指针。内部搜索用指针，并非调度器指针
-        self.mask = Mask(dims, 1)   # 设置屏蔽看门狗
-
-    def set_pointer(self, ptr):
-        self.ptr = ptr
-        self.step = 0
-
-    def record(self, pos):
-        """记录当前位置获得Grant"""
-        self.mask.mask(pos)
-
-        if self.step == 1:
-            self.step = 2
-            # idx = (self.pointer.pointer2index(self.ptr) + self.search_cnt-1) % len(self.order)
-            idx = self.pointer.search2index(self.ptr, self.search_cnt-1)
-            self.next_ptr = self.pointer.next_pointer(idx)
-            # pointer = math.ceil(idx/self.pointer_step)
-            # self.next_pointer = (pointer + 1) * self.pointer_step % len(self.order)
-
-        # return self.next_pointer # 返回新指针位置
+    def pointer(self, val):
+        self.ptr = val
 
     def __iter__(self):
-        self.mask.reset()
         self.search_cnt = 0
-        
-        if self.step == 2:
-            self.ptr = self.next_ptr
-        self.step = 1
-
         return self
 
     def __next__(self):
-        if self.search_cnt < self.select_num:   # 搜索一圈
-            # idx = (self.pointer.pointer2index(self.ptr) + self.search_cnt) % len(self.order)    # 搜索位置Round Robin
-            idx = self.pointer.search2index(self.ptr, self.search_cnt)
+        if self.search_cnt < self.search_num:
+            idx = (self.pointer2index(self.ptr) + self.search_cnt) % self.scope
             pos = self.order[idx]  # 获取当前指针位置
+            ptr = self.index2pointer(idx)
+
             self.search_cnt += 1    # 搜索过的位置个数加1
 
-            if self.mask.is_mask(pos):  # 若位置被屏蔽，则遍历下一个节点
-                return self.__next__()
-
-            return pos
+            return (ptr, pos)
         else:
             raise StopIteration()
+
+
+class Algorithm:
+    def __init__(self, dims=[], order=[], search_num=-1, gnum=1):
+        self.dims = dims if dims!= [] else order2dim(order)
+
+        self.order = Order(self.dims, order, search_num)    # Order组件例化
+        self.mask = Mask(self.dims, gnum)   # Mask组件例化
+
+        self.first_grant = True
+        self.pointer = self.ptr = 0    # 指针和下一跳指针
+
+        self.order_ite = None
+
+    def __iter__(self):
+        self.first_grant = True
+        self.order.pointer(self.pointer)
+        self.mask.reset()
+        self.order_ite = iter(self.order)
+        return self
+
+    def __next__(self):
+        (self.ptr, pos) = next(self.order_ite)
+        (stop, mask) = self.mask.is_mask(pos)
+        if stop:
+            raise StopIteration()
+        elif mask:
+            return self.__next__()
+        else:
+            return pos
+
+    def record(self, pos):
+        self.mask.mask(pos)
+        if self.first_grant:
+            self.pointer = self.ptr
+            self.first_grant = False
 
 
 class Scheduler:
@@ -192,13 +212,10 @@ class Scheduler:
         return rst
 
 
-
-
-
 sch = Scheduler([4,4])
 
 req = [[4,0,0,0], [4,4,0,0], [4,4,4,0],[4,4,8,0]]
-# # req = [[4,4,4,4], [4,4,4,4], [4,4,4,4],[4,4,4,4]]
+# req = [[4,4,4,4], [4,4,4,4], [4,4,4,4],[4,4,4,4]]
 # # req = [[2,2,2,2], [7,7,7,7], [4,4,4,4],[3,3,3,3]]
 qm = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
@@ -227,7 +244,7 @@ qm = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 # ]
 
 
-for _ in range(100):
+for _ in range(1000):
 
 
     for r in range(4):
