b21a22c96eeb4037610546402580a57b8ee5f966
xwHan0 <xwhan2015@outlook.com>
2020-10-12 00:06:38 +0800

    初稿上传

---
+++ python/comlib/tree.py
@@ -0,0 +1,43 @@
+
+from comlib.utilis.argument import series_argument_proc
+
+class Node:
+    """树节点定义"""
+    def __init__(self, **props):
+        self.props = props
+        self.childNodes = []
+        self.parents = []
+
+    def getAttribute(self, key, default=None): return self.props.get([key], default)
+    def setAttribute(self, key, value): self.props[key] = value
+
+    def getChild(self, idx): return self.childNodes[idx]
+    def setChild(self, idx, child): self.childNodes[idx] = child
+
+    def __getitem__(self, key):
+        if isinstance( key, int ):
+            return self.childNodes[key]
+        else:
+            return self.props[key]
+
+    def __setitem__(self, key, value):
+        if isinstance( key, int ):
+            self.childNodes[key] = value
+        else:
+            self.props[key] = value
+
+    def append_child( self, *children ):
+        children = series_argument_proc( children )
+        for child in children:
+            self.childNodes.append( child )
+        return self
+
+    def append_parent( self, *parents ):
+        parents = series_argument_proc( parents )
+        for parent in parents:
+            self.parents.append( parent )
+        return self
+
+    def __iter__(self):
+        return iter( self.childNodes )
+
