7f243e2265e69811811bcf9e936adb2679f79651
xwHan0 <xwhan2015@outlook.com>
2020-10-14 06:57:13 +0800

    添加map函数

--- python/comlib/tree.py
+++ python/comlib/tree.py
@@ -1,6 +1,16 @@
 
 from comlib.utilis.argument import series_argument_proc
 
+#### 异常
+class ErrorReturnType(Exception): pass
+
+
+class NodeMapReturn:
+    def __init__(self, *nodes):
+        self.nodes = nodes
+
+
+
 class Node:
     """树节点定义"""
     def __init__(self, **props):
@@ -64,6 +74,42 @@ class Node:
             data = post( self, lvls=lvls, data=data )
         
         return data
+
+    def map(self, prev, post=None, udata={}, lvls=[]):
+        """遍历整个树
+
+        Arguments:
+        * prev: {(node,udata,lvls)=>udata, List<Node>} ---- Prev过程处理函数
+        * post: {(node,nnode,lvls)=>List<Node>} ---- Post过程处理函数
+            * None: 不进行Post处理，Prev处理结果作为最终结果
+            * Function: 参考`prev`
+        * udata: {dict} ---- 自顶向下传递的参数
+        * lvls: {list} ---- 位置层次序号
+
+        Return:
+        * udata: {dict} ---- 自顶向下传递的参数
+        * nodes: {List<Node>} ---- 新的节点
+        """
+
+        # prev处理
+        udata, new_nodes = prev( self, udata=udata, lvls=lvls )
+
+        # 子节点处理
+        new_children = []
+        for i,child in enumerate(self.childNodes):
+            udata, child_nodes = child.map( prev, post, udata=udata, lvls=lvls+[i] )
+            for c in child_nodes:
+                new_children.append( c )
+            
+        # 追加子节点
+        for c in new_nodes:
+            c.childNodes = new_children
+
+        # post处理
+        if post:
+            new_nodes = post( self, nnode=new_nodes, lvls=lvls )
+
+        return udata, new_nodes
                 
 
 
